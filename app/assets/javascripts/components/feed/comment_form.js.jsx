2a7f13041f90ece3315deecec8070957da90e647
Gregory Kohler <gregorykohler@gmail.com>
2016-02-27 20:29:22 -0800

    prettify comment form

--- app/assets/javascripts/components/feed/comment_form.js.jsx
+++ app/assets/javascripts/components/feed/comment_form.js.jsx
@@ -17,16 +17,28 @@
     },
 
     render: function () {
+      var thumbnailStyle = {
+        height:"20px",
+        width:"20px",
+        border: "1px solid rgba(0, 0, 0, 0.15)",
+        position:"absolute",
+        left: "20px",
+        backgroundImage: "url(" + CURRENT_USER_IMAGE_URL + ")",
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+      };
+
       return (
         <div className="song-comment-form">
+          <div className="comment-thumbnail" style={thumbnailStyle}/>
           <input type="text"
-                 placeholder="Add comment"
+                 placeholder="Write a comment"
                  onKeyUp={this.keyUpHandler}
                  valueLink={this.linkState("commentBody")}/>
         </div>
-      )
+      );
     }
-  })
+  });
 
 
 }(this));
